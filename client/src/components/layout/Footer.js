27e6ba53c7744f308ab799bc788b2e72de9cb1b9
Adrian Dixon <drix3991@gmail.com>
2018-12-29 02:58:07 -0500

    Finally Done Deploy FS Masterclass App!!

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <footer className="bg-blue text-white mt-5 p-4 text-center">
+            Copyright &copy; {new Date().getFullYear()} <a className="copyright-name" href="mailto:ar_dixon@live.com">Adrian Dixon</a>
+        </footer>
+    );
+};
---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+
+class Landing extends Component {
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/feed');
+    }
+  }
+
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">Mini Thread</h1>
+                <p className="lead">
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
+                  Login
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Landing.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(Landing);
