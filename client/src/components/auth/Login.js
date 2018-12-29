b491052ec8a06b4087d18c4e46b69d2beafc3cfd
Adrian Dixon <drix3991@gmail.com>
2018-12-29 03:00:06 -0500

    Finally Done Deploy FS Masterclass App!!

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -78,6 +78,7 @@ class Login extends Component {
                   onChange={this.onChange}
                   error={errors.password}
                 />
+
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
             </div>
